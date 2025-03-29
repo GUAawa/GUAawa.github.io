8b56b31a2b91afdd78b6a41a3089b4b5a22e9dc4
GUAawa <zhezhishiguaeryi@qq.com>
2025-03-29 19:12:37 +0800

    description

--- PengShu/Solver/scripts/generator.js
+++ PengShu/Solver/scripts/generator.js
@@ -28,8 +28,7 @@ class Situation {
         this.to = [];
     }
 }
-
-//generate situations & assess border
+//generate situations & assess winning
 /**
  * @type {Situation[]}
  */
@@ -107,9 +106,15 @@ for (let a = 1; a < 10; a++) {
     }
 }
 //#endregion
+//人工标记situation
+// a|x 型死循环
+situations[902].assessment = "C";
+situations[206].assessment = "C";
+// 杂死循环
+// situations[124].assessment = "C";
+
 
 console.log(situations);
-
 /**
  * transition_table[from][to] = has_access:bool
  */
@@ -141,95 +146,95 @@ function get_id(type, vals_me, vals_enemy) {
 for (let s of legal_ids) {
     if (situations[s].type == "a|x") {
         let a = situations[s].vals_me[0], x = situations[s].vals_enemy[0];
-        if(a+x!=10){
-            let t = get_id("a|x",[x],[(a+x)%10]);
+        if (a + x != 10) {
+            let t = get_id("a|x", [x], [(a + x) % 10]);
             situations[s].to.push(t);
             situations[t].from.push(s)
             transition_table[s][t] = true;
         }
     } else if (situations[s].type == "ab|x") {
         let a = situations[s].vals_me[0], b = situations[s].vals_me[1], x = situations[s].vals_enemy[0];
-        if(a+x!=10){
-            let t = get_id("a|xy",[x],[(a+x)%10,b]);
+        if (a + x != 10) {
+            let t = get_id("a|xy", [x], [(a + x) % 10, b]);
             situations[s].to.push(t);
             situations[t].from.push(s)
             transition_table[s][t] = true;
-        }else{
-            let t = get_id("a|x",[x],[b]);
+        } else {
+            let t = get_id("a|x", [x], [b]);
             situations[s].to.push(t);
             situations[t].from.push(s)
             transition_table[s][t] = true;
         }
-        if(b+x!=10){
-            let t = get_id("a|xy",[x],[a,(b+x)%10]);
+        if (b + x != 10) {
+            let t = get_id("a|xy", [x], [a, (b + x) % 10]);
             situations[s].to.push(t);
             situations[t].from.push(s)
             transition_table[s][t] = true;
-        }else{
-            let t = get_id("a|x",[x],[a]);
+        } else {
+            let t = get_id("a|x", [x], [a]);
             situations[s].to.push(t);
             situations[t].from.push(s)
             transition_table[s][t] = true;
         }
     } else if (situations[s].type == "a|xy") {
         let a = situations[s].vals_me[0], x = situations[s].vals_enemy[0], y = situations[s].vals_enemy[1];
-        if(a+x!=10){
-            let t = get_id("ab|x",[x,y],[(a+x)%10]);
+        if (a + x != 10) {
+            let t = get_id("ab|x", [x, y], [(a + x) % 10]);
             situations[s].to.push(t);
             situations[t].from.push(s)
             transition_table[s][t] = true;
         }
-        if(a+y!=10){
-            let t = get_id("ab|x",[x,y],[(a+y)%10]);
+        if (a + y != 10) {
+            let t = get_id("ab|x", [x, y], [(a + y) % 10]);
             situations[s].to.push(t);
             situations[t].from.push(s)
             transition_table[s][t] = true;
         }
     } else if (situations[s].type == "ab|xy") {
-        let a = situations[s].vals_me[0], 
-        b = situations[s].vals_me[1], 
-        x = situations[s].vals_enemy[0], 
-        y = situations[s].vals_enemy[1];
-        if(a+x!=10){
-            let t = get_id("ab|xy",[x,y],[(a+x)%10,b]);
+        let a = situations[s].vals_me[0],
+            b = situations[s].vals_me[1],
+            x = situations[s].vals_enemy[0],
+            y = situations[s].vals_enemy[1];
+        if (a + x != 10) {
+            let t = get_id("ab|xy", [x, y], [(a + x) % 10, b]);
             situations[s].to.push(t);
             situations[t].from.push(s)
             transition_table[s][t] = true;
-        }else{
-            let t = get_id("ab|x",[x,y],[b]);
-            situations[s].to.push(t);
-            situations[t].from.push(s)
-            transition_table[s][t] = true;
-        }
-        if(b+x!=10){
-            let t = get_id("ab|xy",[x,y],[a,(b+x)%10]);
-            situations[s].to.push(t);
-            situations[t].from.push(s)
-            transition_table[s][t] = true;
-        }else{
-            let t = get_id("ab|x",[x,y],[a]);
+        } else {
+            let t = get_id("ab|x", [x, y], [b]);
             situations[s].to.push(t);
             situations[t].from.push(s)
             transition_table[s][t] = true;
         }
-        if(a+y!=10){
-            let t = get_id("ab|xy",[x,y],[(a+y)%10,b]);
+        if (b + x != 10) {
+            let t = get_id("ab|xy", [x, y], [a, (b + x) % 10]);
             situations[s].to.push(t);
             situations[t].from.push(s)
             transition_table[s][t] = true;
-        }else{
-            let t = get_id("ab|x",[x,y],[b]);
+        } else {
+            let t = get_id("ab|x", [x, y], [a]);
             situations[s].to.push(t);
             situations[t].from.push(s)
             transition_table[s][t] = true;
         }
-        if(b+y!=10){
-            let t = get_id("ab|xy",[x,y],[a,(b+y)%10]);
+        if (a + y != 10) {
+            let t = get_id("ab|xy", [x, y], [(a + y) % 10, b]);
             situations[s].to.push(t);
             situations[t].from.push(s)
             transition_table[s][t] = true;
-        }else{
-            let t = get_id("ab|x",[x,y],[a]);
+        } else {
+            let t = get_id("ab|x", [x, y], [b]);
+            situations[s].to.push(t);
+            situations[t].from.push(s)
+            transition_table[s][t] = true;
+        }
+        if (b + y != 10) {
+            let t = get_id("ab|xy", [x, y], [a, (b + y) % 10]);
+            situations[s].to.push(t);
+            situations[t].from.push(s)
+            transition_table[s][t] = true;
+        } else {
+            let t = get_id("ab|x", [x, y], [a]);
             situations[s].to.push(t);
             situations[t].from.push(s)
             transition_table[s][t] = true;
@@ -240,92 +245,135 @@ for (let s of legal_ids) {
 //transition table
 //situation to
 //situation from
-
 const max_loop = 1000;
 let loop_times = 0
-while(true){
+while (true) {
     loop_times++;
     let have_change = false; //这一轮有没有出现新的评估
     let all_assessed = true; //所有局面都已评估
     let origin_reached = false; //初始局面已经被评估
 
-    for(let s of legal_ids){
+    for (let s of legal_ids) {
         let S = situations[s];
-        if(S.assessment != null) continue; //already assessed
+        if (S.assessment != null) continue; //already assessed
         all_assessed = false;
         //rule1 have L in tos, W
         //rule2 all tos are W, L
+        //rule3 as all tos are W or C, choose C
         let win_count = 0;
-        for(let t of S.to){
+        let cycle_count = 0;
+        for (let t of S.to) {
             let T = situations[t];
-            if(T.assessment == "L"){ //rule1
+            if (T.assessment == "L") { //rule1
                 S.assessment = "W";
                 have_change = true;
                 break;
             }
-            else if(T.assessment == "W"){
+            else if (T.assessment == "W") {
                 win_count++;
             }
+            else if (T.assessment == "C") {
+                cycle_count++;
+            }
         }
-        if(win_count == S.to.length){
+        if (win_count == S.to.length) {
             S.assessment = "L";
+            have_change = true;
+            break;
+        }
+        else if (win_count+cycle_count == S.to.length) { //else说明win不够了，这里如果满足了cycle就显然不等于0
+            S.assessment = "C";
             have_change = true;
             break;
         }
     }
 
-    if(situations[1111].assessment != null) origin_reached = true;
+    if (situations[1111].assessment != null) origin_reached = true;
 
-    if(!have_change){console.log("no change anymore"); break;}
-    if(all_assessed){console.log("all assessed"); break;}
-    if(origin_reached){console.log("origin reached"); break;}
-    if(loop_times >= max_loop){console.log("max loop reached"); break;}
+    if (!have_change) { console.log("no change anymore"); break; }
+    if (all_assessed) { console.log("all assessed"); break; }
+    if (origin_reached) { console.log("origin reached"); break; }
+    if (loop_times >= max_loop) { console.log("max loop reached"); break; }
 }
 
-function get_tos(s){
+function get_tos(s) {
     let l = [];
-    for(let id of situations[s].to){
+    for (let id of situations[s].to) {
         l.push(situations[id])
     }
     return l
 }
-
-function analysis_loop(s,list = []){ //简单的数学，我们能发现null的唯一解释就是loop，即null的必定是nullloop的一环，自己的tos必然存在null
-    if(list.length != 0 && s == list[0]){console.log(1); return list};
-    if(list.length == 0) list.push(s);
+// function analysis_loop(s, list = []) { //简单的数学，我们能发现null的唯一解释就是loop，即null的必定是nullloop的一环，自己的tos必然存在null
+//     if (list.length != 0 && s in list) { return list };
+//     if (list.length == 0) list.push(s);
+//     let tos = get_tos(s)
+//     for (let T of tos) {
+//         if (T.assessment == null) {
+//             console.log(T.id);
+//             list.push(T.id);
+//             return analysis_loop(T.id, list);
+//         }
+//     }
+// }
+let list = []; //参与null循环的状态
+function analysis_loop(s, list = null) { //简单的数学，我们能发现null的唯一解释就是loop，即null的必定是nullloop的一环，自己的tos必然存在null
+    if (list == null) list = [s];
+    if (list.length != 0 && s in list) { return list };
     let tos = get_tos(s)
-    for(let T of tos){
-        if(T.assessment == null){
+    for (let T of tos) {
+        if (T.assessment == null) {
             console.log(T.id);
             list.push(T.id);
-            return analysis_loop(T.id,list);
+
         }
     }
 }
-
-function help(s){
+function help(s) {
     let assessment = situations[s].assessment;
     let suggestion;
-    if(assessment == null){
-        suggestion = "我们暂时没有找到定数，这种情况会在对局进一步发展后得到解决。";
-    }else if(assessment == "L"){
+    let decision = "";
+    if (assessment == null) {
+        suggestion = "我们无力评估，这种情况会在对局进一步发展后得到解决。";
+    } else if (assessment == "L") {
         suggestion = "如果对方很聪明，你会输。期待他失误吧。";
-    }else if(assessment == "W"){
-        suggestion = "下一步："
-        for(let t of situations[s].to){
-            if(situations[t].assessment == "L"){
+    } else if (assessment == "C"){
+        suggestion = "你们陷入了循环，如果双方都很聪明，平局。我会引导你不受压制。";
+        for (let t of situations[s].to) {
+            if (situations[t].assessment == "C") {
                 // console.log(t);
-                suggestion += t.toString() + "; "
+                decision += t.toString() + "; "
+            }
+        }
+    } else if (assessment == "W") {
+        suggestion = "跟我走，你就赢定了。"
+        for (let t of situations[s].to) {
+            if (situations[t].assessment == "L") {
+                // console.log(t);
+                decision += t.toString() + "; "
             }
         }
     }
     return {
         assessment,
-        suggestion
+        suggestion,
+        decision,
     }
 }
-
-//let i = 0;for(let id of legal_ids){if(situations[id].assessment!=null)i++;}console.log(i);
+// let i = 0;for(let id of legal_ids){if(situations[id].assessment!=null)i++;}console.log(i); //已评估数量
+// let l = [];
+// for(let s of legal_ids){
+//     let S = situations[s];
+//     if(S.type == "a|x"){
+//         if(S.assessment == null){
+//             l.push(s);
+//         }
+//     }
+// }
+// console.log(l);
+// delete l;
 //人工记录
 /*
  */
+function t(id){
+    return [situations[id].assessment,situations[id].to.toString()]
+}
