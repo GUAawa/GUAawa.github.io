b5b135317f1d4be0dd58db0b2275a346a44d595d
GUAawa <zhezhishiguaeryi@qq.com>
2025-05-13 21:17:32 +0800

    description

--- OurBoard/scripts/index.js
+++ OurBoard/scripts/index.js
@@ -4,13 +4,13 @@ const DOM_content = document.getElementById("content");
 async function handleGet(){
     const content = await TextCC.get(numid);
     DOM_content.value = content;
-    setTimeout(()=>alert("获取了哟~"),1);
+    setTimeout(()=>alert("获取了哟~"),10);
 }
 
 async function handleSubmit(){
     const content = DOM_content.value;
     await TextCC.set(numid,content);
-    setTimeout(()=>alert("提交了哟~"),1);
+    setTimeout(()=>alert("提交了哟~"),10);
 }
 
 handleGet()
